cd0b2a9e74da8962ee22695a7a1bd0cd7b145d31
RAVI KUMAR <rk1403307@outlook.com>
2020-11-17 20:33:47 +0530

    Code changes for registration

--- DemoProjectCore/Controllers/HomeController.cs
+++ DemoProjectCore/Controllers/HomeController.cs
@@ -3,28 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DemoProjectCore.Models;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DemoProjectCore.Controllers
 {
+    
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Login(UserLoginViewModel userLoginViewModel)
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+        public ActionResult UserRegistration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UserRegistration(UserIdentityViewModel userIdentityViewModel)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Sql"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("Insert into dbo.UserIdentity(User_Name,EmailId,Phone_No,Address,IsActive,CreatedBy,CreatedDateTime,PassWord) Values(@name,@email,@Ph,@Add,@IsA,@CB,@CDT,@Pass)", con);
+            cmd.Parameters.Add(new SqlParameter("@name", userIdentityViewModel.User_Name));
+            cmd.Parameters.Add(new SqlParameter("@email", userIdentityViewModel.EmailId));
+            cmd.Parameters.Add(new SqlParameter("@Ph", userIdentityViewModel.Phone_No));
+            cmd.Parameters.Add(new SqlParameter("@Add", userIdentityViewModel.Address)); 
+            cmd.Parameters.Add(new SqlParameter("@IsA", userIdentityViewModel.IsActive = true));
+            cmd.Parameters.Add(new SqlParameter("@CB", userIdentityViewModel.CreatedBy = 1));
+            cmd.Parameters.Add(new SqlParameter("@CDT", userIdentityViewModel.CreatedDateTime = DateTime.Now));
+            cmd.Parameters.Add(new SqlParameter("@Pass", userIdentityViewModel.Password));
+            
+            if(userIdentityViewModel.Password == userIdentityViewModel.Confirm_Password)
+            {
+                con.Open();
+                int noOfRowAffected = cmd.ExecuteNonQuery();
+                con.Close();
+                if (noOfRowAffected > 0)
+                {
+                    ViewBag.Success = "Registration Successful";
+                    return View();
+                }
+                else
+                {
+                    ViewBag.Warning = "Try again";
+                    return View();
+                }
+            }
+            else
+            {
+                ViewBag.Warning = "Please check your Password again";
+                return View();
+            }
         }
     }
 }
---
+++ DemoProjectCore/Models/UserIdentityViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoProjectCore.Models
+{
+    public class UserIdentityViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string User_Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string EmailId { get; set; }
+        [Required]
+        public string Phone_No { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public bool IsActive { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        [Required]
+        public string  Password{ get; set; }
+        [Required]
+        [Compare("Password")]
+        public string Confirm_Password { get; set; }
+    }
+}
